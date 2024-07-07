ba0e01ca33342a203a3b9ab3d85f7fdcc793df6e
Zernez <fernando.pannullo@yahoo.it>
2024-07-07 20:45:16 +0200

    Add terraform features

--- terraform/helm_release.tf
+++ terraform/helm_release.tf
@@ -14,9 +14,9 @@ provider "helm" {
 }
 
 resource "helm_release" "goapphelm" {
-  name         = "helm"
+  name         = "helmapp"
   repository   = "https://zernez.github.io/K8s-helm-terraform-docker-go"
-  chart        = "helm"
+  chart        = "helmapp"
 
   values = [
     file("../helm/values.yaml"),
